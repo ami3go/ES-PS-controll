a8a6dc3fb5b3930d6e5993548c9d74483bc4f571
ami3go <69671996+ami3go@users.noreply.github.com>
2022-05-13 11:26:18 +0300

    Update ES-PS-controll_on_off.py

--- ES-PS-controll_on_off.py
+++ ES-PS-controll_on_off.py
@@ -1,10 +1,19 @@
+# more information on https://pypi.org/project/ea-psu-controller/
+# installation: pip3 install ea_psu_controller
+"""
+
+    EA PS power supply control script
+    Simple ON/ OFF cycling to check device durability.
+
+"""
+
 import ea_psu_controller
 import time
-# more information on https://pypi.org/project/ea-psu-controller/
+
+# please define your COM port
+# USB\VID_232E&PID_0010\
 
 ps_com_port = 'COM13'
-out_voltage = 0
-
 
 ps_name = ea_psu_controller.PsuEA.PSU_DEVICE_LIST_WIN
 
@@ -12,11 +21,13 @@ ps_name = "PS 2042-10B"
 print(f'Power Supply name: {ps_name}')
 
 print(f'Connecting to  {ps_com_port}')
-# ps = ea_psu_controller.PsuEA(ps_name)
 ps = ea_psu_controller.PsuEA(comport=ps_com_port)
-txt = ps.get_device_description()
-print(f'Connected to  {txt}')
+print(f'Connected to  {ps.get_device_description()}')
 time.sleep(0.5)
+
+# initial voltage of power supply
+out_voltage = 0
+
 
 try:
     print(f'Switching remote control on')
@@ -29,31 +40,45 @@ try:
 
     print(f'Turning on output')
     ps.output_on()
-    Vout_high = 15
-    Vout_low = 1
+
+    Vout_high = 15  # Set a HIGH voltage
+
+    Vout_low = 1  # Set LOW voltage , Set 0 for Switch off
+
+    Nreps = 1000  # number of cycles, Better to use for cycle to prevent software stuck
+
+    ONtime = 5  # on time in seconds 
+
+    OFFtime = 5  # off time in seconds
+
     vout = Vout_low
-    Nreps = 1000
-    ONtime = 2
-    OFFtime = 1
+
+    """
+    ***** Cycling start ***** 
+    """
+
     for x in range(0, Nreps):
 
         ps.set_voltage(Vout_high)
         time.sleep(ONtime)
+        #
         print(f"Counter: {x}, Voltage: {round(ps.get_voltage(),3)}, Current: {round(ps.get_current(),3)}")
         ps.set_voltage(Vout_low)
         time.sleep(OFFtime)
 
-
+    # disconnect and stop communication
     time.sleep(2)
     ps.output_off()
     ps.close()
 
 
-
+# in case something got wrong
 except Exception as e:
+    print("*"*50)
     print(e)
     print(f'close connection...')
     print("close remote control ")
-    ps.remote_off()
     print("close connection ")
+    print("*"*50)
+    ps.remote_off()
     ps.close(remote=False, output=False)
